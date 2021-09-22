bd0127a2512eeecc44343c3746fa331ed1c4b7c6
Jideije Emeka <jideije.emeka@gmail.com>
2021-09-22 14:33:55 -0700

    added the admin login in the first screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,9 @@
+import 'package:event_app/screens/admin_login.dart';
+import 'package:event_app/screens/drawer.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
-  const Home({ Key? key }) : super(key: key);
+  const Home({Key? key}) : super(key: key);
 
   @override
   _HomeState createState() => _HomeState();
@@ -10,8 +12,32 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Event App'),
+      ),
+      drawer: MainDrawer(),
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal: 20),
+        child: Center(
+          child: Column(children: [
+            SizedBox(
+              height: 10,
+            ),
+            Text('Events Registration System',
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+            SizedBox(
+              height: 5,
+            ),
+            Text('With QR Code Generator and QR  Scanner'),
+            SizedBox(
+              height: 50,
+            ),
+            //ADMIN LOGIN
+            AdminLogin(),
+          ]),
+        ),
+      ),
     );
   }
 }
