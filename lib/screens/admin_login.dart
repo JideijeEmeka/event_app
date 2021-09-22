bea7cdb0986693de2fbdfc46b1a94fa155737225
Jideije Emeka <jideije.emeka@gmail.com>
2021-09-22 14:32:28 -0700

    designed the admin login form

---
+++ lib/screens/admin_login.dart
@@ -0,0 +1,46 @@
+import 'package:event_app/customwidgets/widgets.dart';
+import 'package:flutter/material.dart';
+
+class AdminLogin extends StatefulWidget {
+  const AdminLogin({Key? key}) : super(key: key);
+
+  @override
+  _AdminLoginState createState() => _AdminLoginState();
+}
+
+class _AdminLoginState extends State<AdminLogin> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 250,
+      width: MediaQuery.of(context).size.width * 0.8,
+      decoration: BoxDecoration(
+          border: Border.all(width: 2, color: Colors.blue),
+          borderRadius: BorderRadius.circular(15)),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "Admin Login",
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: customTextField("email", "your email")),
+          SizedBox(
+            height: 15,
+          ),
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: customTextField("password", "your password")),
+          SizedBox(
+            height: 15,
+          ),
+          customButton("Login"),
+        ],
+      ),
+    );
+  }
+}
