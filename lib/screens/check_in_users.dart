216cb5c171c1c7213b03d47ed8c1cd9580b42618
Jideije Emeka <jideije.emeka@gmail.com>
2021-09-22 18:34:36 -0700

    designed the check in users screen

---
+++ lib/screens/check_in_users.dart
@@ -0,0 +1,66 @@
+import 'package:event_app/screens/drawer.dart';
+import 'package:flutter/material.dart';
+
+class CheckedIn extends StatefulWidget {
+  const CheckedIn({Key? key}) : super(key: key);
+
+  @override
+  _CheckedInState createState() => _CheckedInState();
+}
+
+class _CheckedInState extends State<CheckedIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Check In Users"),
+      ),
+      drawer: MainDrawer(),
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal: 20),
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              Text('Total Registered Users',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              SizedBox(
+                height: 20,
+              ),
+              Text('Backend Number Goes Here',
+                  style: TextStyle(
+                    fontSize: 18,
+                  )),
+              SizedBox(
+                height: 40,
+              ),
+              Text('Total Checked in Users',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              SizedBox(
+                height: 20,
+              ),
+              Text('Backend Number Goes Here',
+                  style: TextStyle(
+                    fontSize: 18,
+                  )),
+              SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Check In"),
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.blue,
+                    padding: EdgeInsets.symmetric(horizontal: 25, vertical: 15),
+                    textStyle:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
