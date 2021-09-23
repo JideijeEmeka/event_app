fa74ebe44781bdf26ee33490445557b339423110
Jideije Emeka <jideije.emeka@gmail.com>
2021-09-23 02:03:09 -0700

    created user model

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:event_app/screens/events_page.dart';
 import 'package:event_app/screens/home.dart';
-import 'package:event_app/screens/user_reg.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
---
+++ lib/models/user.dart
@@ -0,0 +1,5 @@
+class MyUser {
+  String userId;
+
+  MyUser({required this.userId});
+}
